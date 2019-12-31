cbff9449d522fb9838104bbd71d8c7ba3e2afcd8
NEU-ZYXi <stephenxi@hotmail.com>
2019-12-31 14:01:04 -0800

    Create 58. Length of Last Word.cpp

---
+++ 58. Length of Last Word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s[i] == ' ') {
+                if (ans != 0) {
+                    break;
+                }
+            } else {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
