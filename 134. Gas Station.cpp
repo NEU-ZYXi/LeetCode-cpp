47784d6e689c1952e8d531f6ac9b9ae3912f4099
NEU-ZYXi <stephenxi@hotmail.com>
2020-01-13 00:05:16 -0800

    Create 134. Gas Station.cpp

---
+++ 134. Gas Station.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = gas.size() - 1, end = 0, sum = gas[start] - cost[start];
+        while (start > end) {
+            if (sum >= 0) {
+                sum += gas[end] - cost[end];
+                end++;
+            } else {
+                start--;
+                sum += gas[start] - cost[start];
+            }
+        }
+        return sum >= 0 ? start : -1;
+    }
+};
